0eee43af69cfd3fca69694f411a4cc5ed6bdbd20
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2021-07-23 21:29:59 +0530

    setting up training scripts and creating REST API endpoints

--- backend/services/text_extraction/application/configuration.py
+++ backend/services/text_extraction/application/configuration.py
@@ -1,6 +1,7 @@
 from injector import Module, singleton
 
 from common.logging.console_loger import ConsoleLogger
+from common.logging.logger import Logger
 from services.text_extraction.settings import Settings
 
 
