f77922d264c6ba8bfc90fa801629267c3cf671b9
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2021-07-24 18:59:30 +0530

    setting up training scripts and creating REST API endpoints

---
+++ backend/services/text_extraction/application/ai/training/src/preprocess.py
@@ -0,0 +1,20 @@
+import re
+import string
+from nltk import word_tokenize
+
+from backend.services.text_extraction.settings import Settings
+
+
+class Preprocess:
+    def __init__(self):
+        self.settings = Settings
+
+    def clean_text(self, text):
+        text = text.lower()
+        text = re.sub('\[.*?\]', '', text)
+        text = re.sub('https?://\S+|www\.\S+', '', text)
+        text = re.sub('<.*?>+', '', text)
+        text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+        text = re.sub('\n', '', text)
+        text = re.sub('\w*\d\w*', '', text)
+        return text
